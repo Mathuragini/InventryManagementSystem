4bb6ed8d86a54b69d9379af334c90e4da4c62cf7
Mathuragini <mathusharma15@gmail.com>
2019-08-20 18:23:11 +0530

    customerCRUDadded

---
+++ src/com/asianpaint/customer/Customer.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asianpaint.customer;
+
+/**
+ *
+ * @author Mathuragini
+ */
+public class Customer {
+    
+    //Declare varaiables for customer class
+    private int customerId;
+    private String customerName;
+    private int tpNumber;
+    private String address;
+    private String email;
+    
+    
+    //Create parametrized constructor for customer class
+    public Customer(int customerId, String customerName, String address, int tpNumber, String email) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.tpNumber = tpNumber;
+        this.address = address;
+        this.email = email;
+    }
+
+    //Generate getters for the customer class varaiables
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public int getTpNumber() {
+        return tpNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
+}
